7eb4db46ab7bfaec761b7bec15b8c9b4b83816a0
Trevor Fountain <doches@dhcp-90-072.inf.ed.ac.uk>
2010-06-21 12:52:18 +0100

    +--fake+ switch for fake corpus/mcrae corpus evaluation

--- scripts/nlda2_eval.rb
+++ scripts/nlda2_eval.rb
@@ -1,9 +1,14 @@
 require 'yaml'
+
+fake = ARGV.include?("--fake")
+ARGV.reject! { |x| x == "--fake" }
+
 results = ARGV.shift
+
 
 assignments = IO.readlines(results).map { |x| x.strip.split(" ").map { |y| y.to_i } }
 
-targets = IO.readlines("scripts/target_words").map { |x| x.strip }
+targets = IO.readlines(fake ? "scripts/fake.target_words" : "scripts/target_words").map { |x| x.strip }
 
 wordmap = IO.readlines("#{results}.wordmap").map { |x| x.strip.split(" ") }
 wordmap_t = wordmap.map { |x| [x[0].to_i,x[1]] }.reject { |x| not targets.include?(x[1]) }
@@ -23,5 +28,5 @@ fout = File.open("#{results}.yaml","w")
 fout.puts categories.to_yaml
 fout.close
 
-score = `clusterval -s -g scripts/mcrae.cluster -c #{results}.yaml`
+score = `clusterval -s -g scripts/#{fake ? 'fake.yaml' : 'mcrae.cluster'} -c #{results}.yaml`
 puts score
