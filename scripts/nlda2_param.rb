758c1ed94645b9510fd709c11617882438536f73
Trevor Fountain <doches@gmail.com>
2010-07-15 16:33:13 +0100

    .

--- scripts/nlda2_param.rb
+++ scripts/nlda2_param.rb
@@ -1,8 +1,10 @@
 # Do genetic algorithms exploration of parameter space for nlda2
 
+require '../../clusterfuck/lib/clusterfuck'
+
 class Instance
-	MaxWindow = 30
-	attr_accessor :alpha, :beta, :gamma, :window
+	MaxWindow = 5
+	attr_accessor :alpha, :beta, :gamma, :window, :score
 	attr_reader :elapsed
 	
 	@@input=nil
@@ -23,32 +25,63 @@ class Instance
 	end
 	
 	def mutate!(rate)
+		@window = rand < 0.5 ? 0 : 1
+		@alpha += rand * 0.2
+		if @alpha < 0.1
+			@alpha = 0.1+rand*0.2
+		end
+		@beta += rand * 0.2
+		if @beta < 0.1
+			@beta = 0.1+rand*0.2
+		elsif @beta > 1.0
+			@beta = 1.0 - rand*0.2
+		end
+		@gamma += rand * 0.2
+		if @gamma < 0.0
+			@gamma = rand*0.2
+		elsif @gamma > 1.0
+			@gamma = 1.0-rand*0.2
+		end
+=begin
 		gene = rand
-		if gene < 0.25
+		if gene < 0.33
+			old_alpha = @alpha
 			@alpha += (rand) * (rand < 0.5 ? -1 : 1)
-			@alpha = 0.001 if @alpha < 0.0
-		elsif gene < 0.5
-			@beta += (rand) * (rand < 0.5 ? -1 : 1)
-			@beta = 0.001 if @beta < 0.0
-		elsif gene < 0.75
+			@alpha = old_alpha+rand if @alpha < 0.0
+		elsif gene < 0.66
+			@beta += (rand - 0.5)*0.1
+			if @beta < 0.1
+				@beta = 0.1+rand*0.2
+			elsif @beta > 1.0
+				@beta = 1.0-rand*0.2
+			end
+		elsif gene < 0.1
 			@gamma += (rand) * (rand < 0.5 ? -1 : 1)
 			@gamma = 0.001 if @gamma < 0.0
 		else
 			@window += (1+rand*5).to_i * (rand < 0.5 ? -1 : 1)
 			@window = 0 if @window < 0.0
 		end
+=end
 	end
 	
 	def breed_with(b)
 		Instance.breed(self,b)
 	end
+
+	def outfile
+		"#{@@input}.results/#{@alpha}_#{@beta}_#{@gamma}_#{@window}"
+	end
+	
+	def job
+		"cd models/corncob; nice ./nlda2 #{@alpha} #{@beta} #{@gamma} #{@@input} #{self.outfile} #{@window};  ruby scripts/nlda2_eval.rb #{self.outfile}"
+	end
 	
 	def score
-		outfile = "#{@@input}.results/#{@alpha}_#{@beta}_#{@gamma}_#{@window}"
 		start = Time.now
-		`./nlda2 #{@alpha} #{@beta} #{@gamma} #{@@input} #{outfile} #{@window}`
+		`./nlda2 #{@alpha} #{@beta} #{@gamma} #{@@input} #{self.outfile} #{@window}`
 		@elapsed = Time.now - start
-		return `ruby scripts/nlda2_eval.rb #{outfile}`.to_f 
+		return `ruby scripts/nlda2_eval.rb #{self.outfile}`.to_f 
 	end
 	
 	def to_s
@@ -64,7 +97,11 @@ class Instance
 	
 	def Instance.random
 		alpha = rand*10
+<<<<<<< HEAD
 		beta = rand*0.8+0.2
+=======
+		beta = rand*0.95+0.5
+>>>>>>> .
 		gamma = rand
 		window = (rand*MaxWindow).to_i
 		
@@ -81,6 +118,7 @@ class World
 		@mutants = mutants
 		@popsize = popsize
 		@best = [nil,0]
+		@input = input
 		
 		Instance.input = input
 		begin
@@ -98,10 +136,58 @@ class World
 		end
 		
 		`mkdir #{Instance.input}.results/` if not File.exists?("#{Instance.input}.results/")
+		@hosts = IO.readlines("scripts/hosts.#{@input}").map { |x| x.strip }
 	end
 	
 	def simulate_generation
 		start_time = Time.now
+=begin		
+		# Compute scores
+		jobs = @population.map { |instance| instance.job }
+		clusterfile = File.open("nlda2_params.clusterfile","w")
+#	task.jobs = #{@population.map { |i| Clusterfuck::Job.new(i.outfile.split("/").pop,i.job) }.inspect}
+#	task.jobnames = #{@population.map { |i| i.outfile.split("/").pop}.inspect}
+		clusterfile.puts <<CF
+Clusterfuck::Task.new do |task|
+	task.hosts = @hosts
+	task.jobs = [#{@population.map { |i| "Clusterfuck::Job.new('#{i.outfile.split('/').pop}','#{i.job} 2> /dev/null')" }.join(", ")}]
+	task.temp = "fragments"
+	task.username = "s0897549"
+	task.password = "d()ches42"
+	task.showreport = false
+	task.verbose=Clusterfuck::VERBOSE_ALL
+end
+CF
+		clusterfile.close
+		`clusterfuck nlda2_params.clusterfile`
+
+		# Rename fragment files
+		cmds = []
+		cwd = "fragments"
+		Dir.foreach(cwd) do |f|
+			if f =~ /^(\d\.\d+)_(\d\.\d+)_(\d\.\d+)_(\d+)/
+				cmds.push "mv #{cwd}/#{f} #{cwd}/#{$1}_#{$2}_#{$3}_#{$4}.score 2> /dev/null"
+			end
+		end
+		cmds.each { |cmd| `#{cmd}` }
+		# Load scores
+		i = 0
+		@population.map do |instance|
+			begin
+				instance.score = IO.readlines("fragments/#{instance.outfile.split("/").pop}.score").join("").strip.to_f
+			rescue
+				instance.score = 0.0
+				begin
+					@hosts.delete_at i
+				rescue
+					STDERR.puts "Could not delete host"
+				end
+				STDERR.puts $!
+			end
+			i += 1
+		end
+		`rm fragments/*`
+=end
 		scores = @population.map { |instance| [instance, instance.score] }.sort { |a,b| b[1] <=> a[1] }	
 		elapsed = Time.now - start_time
 		STDERR.puts ""
@@ -137,7 +223,7 @@ class World
 end
 
 if __FILE__ == $0
-	pool = World.new(ARGV.shift,10,3)
+	pool = World.new(ARGV.shift,6,3)
 	100.times { pool.simulate_generation }
 	puts "-----------------------------------"
 	puts "Best result after #{pool.generation-1} generations:"
