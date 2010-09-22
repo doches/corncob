d30bc8dcd0e1c1a480803d2ac2d9081f89509ce9
Trevor Fountain <doches@gmail.com>
2010-09-22 15:14:59 +0100

    Better graphs; warn on malformed input

--- scripts/graph_scores.rb
+++ scripts/graph_scores.rb
@@ -9,14 +9,24 @@ output = ARGV.shift
 output = "#{output}.png" if not output =~ /\.png$/
 
 scores = {}
+corpus = nil
+threshold = nil
 Dir.foreach(input) do |file|
 	if file =~ /fscore$/ and file =~ pattern
 		components = file.split(".")
 		if components.size == 4
 			STDERR.puts file
-			corpus = components.shift
-			docindex = components.shift
-			threshold = components.shift
+			_corpus = components.shift
+			if not corpus.nil? and corpus != _corpus
+				STDERR.puts "[warn] Multiple corpora in plot (#{corpus} and #{_corpus})"
+			end
+			corpus = _corpus
+			docindex = components.shift.to_i
+			_threshold = components.shift.gsub("_",".").to_f
+			if not threshold.nil? and threshold != _threshold
+				STDERR.puts "[warn] Multiple thresholds in plot (#{threshold} and #{_threshold})"
+			end
+			threshold = _threshold
 			fscore = `cat #{File.join(input,file)}`.to_f
 			scores[docindex] = fscore
 		end
@@ -27,16 +37,17 @@ fout = File.open("plot.plt","w")
 fout.puts <<GNU
 set terminal png
 set output \"#{output}\"
+set xtics rotate by -45
 set yrange [0:0.5]
 set xrange [0:#{scores.size+1}]
-plot 'plot.dat' using 2:xticlabels(1) with linespoints
+plot 'plot.dat' using 2:xticlabels(1) with linespoints title '#{corpus} #{threshold}'
 GNU
 fout.close
 
 fout = File.open("plot.dat","w")
-scores.each_pair do |lines,score|
+scores.map { |l,s| [l,s] }.sort { |a,b| a[0] <=> b[0] }.each do |lines,score|
 	fout.puts "#{lines}\t#{score}"
 end
 fout.close
 
-`gnuplot plot.plt`
+`gnuplot plot.plt && gthumb #{output}`
