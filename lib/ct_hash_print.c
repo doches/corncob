cd7a631451d206eebc6fa78f4e99560cf1b6848a
Trevor Fountain <doches@gmail.com>
2010-10-13 12:02:04 +0100

    Added labeled_fprintf for ct_hash, focw now prints labeled reps
    Also added a script, `preview_ppmi.rb`, for viewing the .reps/.ppmi
    files output by focw

---
+++ lib/ct_hash_print.c
@@ -0,0 +1,24 @@
+#include "ct_hash_print.h"
+
+WordMap *static_hash_print_wordmap;   
+FILE *static_hplh_file;   
+void hash_print_labeled_helper(hash_element *element) 
+{ 
+  fprintf(static_hplh_file,"%s (%d), ",WordMap_reverse_lookup(static_hash_print_wordmap, element->key),element->value);   
+} 
+
+void hash_print_labeled(ct_hash *hash,char *label,WordMap *wordmap)   
+{ 
+  hash_fprint_labeled(stdout,hash,label,wordmap); 
+} 
+
+void hash_fprint_labeled(FILE *fout, ct_hash *hash, char *label, WordMap *wordmap)
+{ 
+  fprintf(fout,"%s <",label); 
+
+  static_hplh_file = fout;
+	static_hash_print_wordmap = wordmap;
+  hash_foreach(hash, &hash_print_labeled_helper); 
+	static_hash_print_wordmap = NULL;   
+  fprintf(fout,">\n");
+}
---
+++ lib/ct_hash_print.h
@@ -0,0 +1,5 @@
+#include "WordMap.h"
+#include "ct_hash.h"
+
+void hash_fprint_labeled(FILE *fout, ct_hash *hash, char *label, WordMap *wordmap);
+void hash_print_labeled(ct_hash *hash,char *label,WordMap *wordmap);
